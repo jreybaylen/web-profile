4e01bf67f4b616677795cfee786fa20b92f22967
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-03-27 10:54:37 -0400

    Task: add page header

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+
+export function Header (): JSX.Element {
+    return (
+        <header>
+            <nav className="container">
+                <ul className="header-list">
+                    <li className="mr-auto">
+                        <Link href="/" className="text-4xl font-bold">
+                            JRMB
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#about-me">
+                            About Me
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#skills">
+                            Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#experiences">
+                            Experiences
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#projects">
+                            Projects
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import Head from 'next/head'
+
+import { Header } from '@components/Header'
+
 type HomeProps = {
 	fonts: string
 }
 
 export default function Home (PROPS: HomeProps): JSX.Element {
 	return (
-		<main className={ PROPS.fonts }>
-			<h1 className="text-3xl">Hi! It&apos;s Jrey!</h1>
-		</main>
+		<>
+			<Head>
+				<title>Jrey&apos;s Web Profile</title>
+				<meta
+					name="description"
+					content="A Software Engineer focused on web and mobile using the latest technologies such as React, React Native, Ionic, and Capacitor"
+				/>
+			</Head>
+			<main className={ PROPS.fonts }>
+				<Header />
+			</main>
+		</>
 	)
 }
